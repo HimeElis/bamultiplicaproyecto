4c09f4fc429832facdb8a0d8a5c7dc1f774caaa1
HimeElis <jpelisburu@gmail.com>
2023-10-19 19:34:23 -0300

    subiendo

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navbar = document.querySelector(".navbar");
+
+    menuToggle.addEventListener("click", function () {
+        navbar.classList.toggle("active");
+    });
+});
+
+
+/*formulario*/
+
+
+
+/*fin formulario*/
